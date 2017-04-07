a6dc3fc58b863f96a3a623e61ea54ffc3982f326
Gürcan Onder <og240447@is227914.intra.cea.fr>
2017-04-07 15:26:16 +0200

    Generate html and wiki

--- ethereum/local-private-network/bin/markdown2mediawiki.sh
+++ ethereum/local-private-network/bin/markdown2mediawiki.sh
@@ -1,5 +1,5 @@
-pandoc -f markdown_github -t mediawiki Setting\ Up\ a\ Local\ Private\ Test\ Network\ with\ One\ Node.md -o Setting\ Up\ a\ Local\ Private\ Test\ Network\ with\ One\ Node.wiki
-sed -i -e 's/&gt;/>/g' Setting\ Up\ a\ Local\ Private\ Test\ Network\ with\ One\ Node.wiki
-sed -i -e 's/&quot;/"/g' Setting\ Up\ a\ Local\ Private\ Test\ Network\ with\ One\ Node.wiki
-sed -i -e 's/<pre class="js">/<source lang="javascript">/g' Setting\ Up\ a\ Local\ Private\ Test\ Network\ with\ One\ Node.wiki
-sed -i -e 's%</pre>%</source>%g' Setting\ Up\ a\ Local\ Private\ Test\ Network\ with\ One\ Node.wiki
+pandoc -f markdown_github -t mediawiki README.md -o README.wiki
+sed -i -e 's/&gt;/>/g' README.wiki
+sed -i -e 's/&quot;/"/g' README.wiki
+sed -i -e 's/<pre class="js">/<source lang="javascript">/g' README.wiki
+sed -i -e 's%</pre>%</source>%g' README.wiki
