93b22296cbe7cad9c071536d7fd0eacccc0307bb
Önder Gürcan <onder.gurcan@gmail.com>
2017-04-07 15:29:17 +0200

    Delete markdown2html.sh

--- ethereum/local-private-network/bin/markdown2html.sh
+++
@@ -1,5 +0,0 @@
-pandoc -f markdown_github README.md -s --toc --highlight-style tango -o index.html
-sed -i -e 's/&gt;/>/g' index.html
-sed -i -e 's/&quot;/"/g' index.html
-sed -i -e 's/<pre class="js">/<source lang="javascript">/g' index.html
-sed -i -e 's%</pre>%</source>%g' index.html
