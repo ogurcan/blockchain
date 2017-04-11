169c0d3b6ae42df8412383c7085cd8aaca63fdae
Önder Gürcan <onder.gurcan@gmail.com>
2017-04-11 14:33:12 +0200

    Update MarketPlace.sol

--- ethereum/local-private-network/contracts/04-MarketPlace-v01/MarketPlace.sol
+++ ethereum/local-private-network/contracts/04-MarketPlace-v01/MarketPlace.sol
@@ -14,13 +14,19 @@ contract MarketPlace {
         address account;
         uint assetBarcode;
         uint stockCount;
+        uint deliverySpeed;
     }
     mapping (address => Vendor) vendors;
-    event VendorRegistered(string name, address account, uint assetBarcode, uint stockCount);
+    event VendorRegistered(string name, address account, uint assetBarcode, uint stockCount, uint deliverySpeed);
 
     // request information
-    address client;
-    uint requestedAssetBarcode;
+    struct Request {
+        address client;
+        uint assetBarcode;
+        uint quantity;
+        uint maxDays;
+    }
+    Request request;
     event AssetRequested(address client, uint barcode);
     
     // bidding information
@@ -43,24 +49,21 @@ contract MarketPlace {
     }  
     
     /* Used by vendors to register themselves. */
-    function registerVendor(string name, uint assetBarcode, uint stockCount) {
+    function registerVendor(string name, uint assetBarcode, uint stockCount, uint deliverySpeed) {
         // if the vendor is already registered, exit the function.
         if (vendors[msg.sender].isRegistered) throw;
         // else register the vendor.
-        vendors[msg.sender] = Vendor(true, name, msg.sender, assetBarcode, stockCount);
-        VendorRegistered(name, msg.sender, assetBarcode, stockCount);
+        vendors[msg.sender] = Vendor(true, name, msg.sender, assetBarcode, stockCount, deliverySpeed);
+        VendorRegistered(name, msg.sender, assetBarcode, stockCount, deliverySpeed);
     }
     
     /* Used by clients to request assets and start bidding.     */ 
-    function requestAsset(uint barcode) {
-        // save the requester
-        client = msg.sender;
-        
-        // save the asset barcode
-        requestedAssetBarcode = barcode;
+    function requestAsset(uint barcode, uint quantity, uint maxDays) {
+        // save the request
+        request = Request(msg.sender, barcode, quantity, maxDays);
         
         // create the event
-        AssetRequested(client, requestedAssetBarcode);
+        AssetRequested(request.client, barcode);
         
         // initialize the bidding process
         expectedProposals = 2;
@@ -71,24 +74,38 @@ contract MarketPlace {
     modifier onlyVendor() { if (vendors[msg.sender].isRegistered) _; }
     
     /* Used by vendors to propose a price for an asset. */
-    function proposePrice(uint barcode, uint price) onlyVendor {
-        // process the proposal
-        PriceProposed(vendors[msg.sender].name, barcode, price);
-        if (price < bestPrice) {
-            bestPrice = price;
-            bestVendor = msg.sender;
-        }
+    function makeOffer(uint barcode, uint price) onlyVendor {
+        Vendor vendor = vendors[msg.sender];
+
+        if (inStock(vendor, barcode)) throw;
+
+        processTheOffer(vendor, barcode, price);
         
         // update bidding condition
         expectedProposals--;
         
         if (expectedProposals == 0) {
-            BiddingFinished(requestedAssetBarcode, bestPrice);
+            BiddingFinished(request.assetBarcode, bestPrice);
         }
     }
     
+    /* return true if there is enough stock */
+    function inStock(Vendor vendor, uint barcode) private returns (bool result) {
+        result = vendor.stockCount >= request.quantity;
+    }
+    
+    /* process the offer to calculate the best offer */
+    function processTheOffer(Vendor vendor, uint barcode, uint price) private {
+        // process the proposal
+        PriceProposed(vendors[msg.sender].name, barcode, price);
+        if (price < bestPrice) {
+            bestPrice = price;
+            bestVendor = msg.sender;
+        }        
+    }
+    
     /* used for checking if a function is called by a registered vendor */
-    modifier onlyClient() { if (msg.sender == client) _; }
+    modifier onlyClient() { if (msg.sender == request.client) _; }
     
     /* The function without name is the default function that is called whenever 
        anyone sends funds to a contract */
@@ -101,11 +118,11 @@ contract MarketPlace {
             // ...to be implemented...
             
             // create the event
-            PaymentReceived(msg.sender, msg.value, requestedAssetBarcode);
+            PaymentReceived(msg.sender, msg.value, request.assetBarcode);
                 
             // ship the asset
             uint trackingNumber = 78623235235;
-            AssetShipped(requestedAssetBarcode, trackingNumber);
+            AssetShipped(request.assetBarcode, trackingNumber);
         }
     }
     
