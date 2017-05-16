54690bbe3c8b8a1e2f5348f998c54cae09946f94
Önder Gürcan <onder.gurcan@gmail.com>
2017-05-16 17:17:59 +0200

    Update ReputationSystem.sol

--- ethereum/local-private-network/contracts/06-ReputationSystem-v01/ReputationSystem.sol
+++ ethereum/local-private-network/contracts/06-ReputationSystem-v01/ReputationSystem.sol
@@ -8,132 +8,132 @@ contract ReputationSystem {
     
     struct Stakeholder {
         string name; // stakeholder name
-        address account; // account address
+        address id; // id as address
         uint profession; // 0 food provider, 1 breeder, 2 alive pig carrier, 3 slaughterhouse, 4 cool meat carrier, 5 brand
     }
     
     mapping (address => Stakeholder) stakeholders;
-     
-    mapping (address => Stakeholder[]) evaluationRelationships;
     
-    struct Business {
-        address foodProviderAccount;
-        address breederAccount;
-        address pigCarrierAccount;
-        address slaughterHouseAccount;
-        address meatCarrierAccount;
-        address brandAccount;
+    // 1st index evaluator profession, 2nd index evaluated profession
+    mapping (uint => mapping(uint => bool)) evaluationRelationships;
+    
+    struct BusinessProcess {
+        address foodProviderID;
+        address breederID;
+        address pigCarrierID;
+        address slaughterHouseID;
+        address meatCarrierID;
+        address brandID;
         uint stage; // 0 no evaluation, 1: breeder evaluated, 2: pig carrier evaluated, 
                     // 3: slaughterhouse evaluated, 4 meat carrier evaluated, 5: brand evaluated
     }
     
-    mapping (uint => Business) businessList;
-    uint businessID = 0;
+    mapping (uint => BusinessProcess) businessProcessList;
+    uint businessProcessID = 0;
     
     struct Evaluation {
+        uint businessProcessID; // for which business process
+        address evaluatorID; // by which stakeholder
+        address evaluatedID; // for which stakeholder
         uint weight; // reputation level of the evaluator at evalution time
         uint score; // score value of the evaluator
     }
     
-    mapping (address => Evaluation[]) evaluations;
+    // Evaluation list holding all evaluations
+    mapping (uint => Evaluation) public evaluations;
+    uint evaluationCount = 0;
    
     /* Constructor */
     function ReputationSystem() {
         // set the owner of this contract
         contractOwner = msg.sender;
+        
+        // since default bool value is false we just need to set the true ones
+        // 1 can evaluate 0 and 2.
+        evaluationRelationships[1][0] = true;
+        evaluationRelationships[1][2] = true;
+        // 2 can evaluate 1.
+        evaluationRelationships[2][1] = true;
+        // 3 can evaluate 1, 2 and 4.
+        evaluationRelationships[3][1] = true;
+        evaluationRelationships[3][2] = true;
+        evaluationRelationships[3][4] = true;
+        // 4 can evaluate 3.
+        evaluationRelationships[4][3] = true;
+        // 5 can evaluate 3 and 4.
+        evaluationRelationships[5][3] = true;
+        evaluationRelationships[5][4] = true;
     }  
     
     /* Add stakeholders to the system. */ 
-    function addStakeholder(string name, address account, uint profession) {
-        stakeholders[account] = Stakeholder(name, account, profession);
-    }
-    
-    /*
-     * 1 can evaluate 0 and 2.
-     * 2 can evaluate 1.
-     * 3 can evaluate 1, 2 and 4.
-     * 4 can evaluate 3.
-     * 5 can evaluate 3 and 4.
-     */
-    function addEvalutionRelationship(address evaluatedAddress) {
-       address evaluatorAddress = msg.sender;
-       Stakeholder evaluator = stakeholders[evaluatorAddress];
-       Stakeholder evaluated = stakeholders[evaluatedAddress];
-       
-       if (evaluator.profession == 1) {
-           if ((evaluated.profession == 0) || (evaluated.profession == 2)) {
-               // add as evaluator
-               evaluationRelationships[evaluator.account].add(evaluated.account);
-           }
-       } else if (evaluator.profession == 2) {
-           if (evaluated.profession == 1) {
-               // add as evaluator
-               evaluationRelationships[evaluator.account].add(evaluated.account);
-           }
-       } else if (evaluator.profession == 3) {
-           if ((evaluated.profession == 1) || (evaluated.profession == 2) || (evaluated.profession == 4)) {
-               // add as evaluator
-               evaluationRelationships[evaluator.account].add(evaluated.account);
-           }
-       } else if (evaluator.profession == 4) {
-           if (evaluated.profession == 3) {
-               // add as evaluator
-               evaluationRelationships[evaluator.account].add(evaluated.account);
-           }
-       } else if (evaluator.profession == 5) {
-           if ((evaluated.profession == 3) || (evaluated.profession == 4)) {
-               // add as evaluator
-               evaluationRelationships[evaluator.account].add(evaluated.account);
-           }
-       }
+    function addStakeholder(string name, address id, uint profession) {
+        stakeholders[id] = Stakeholder(name, id, profession);
     }
     
     /* Create a business between stakeholders */
-    function createBusiness(address foodProviderAccount, address breederAccount, address pigCarrierAccount, 
-                            address slaughterHouseAccount, address meatCarrierAccount, address brandAccount) {
-        if ((stakeholders[foodProviderAccount].profession == 0) && (stakeholders[breederAccount].profession == 1) &&
-            (stakeholders[pigCarrierAccount].profession == 2) && (stakeholders[slaughterHouseAccount].profession == 3) &&
-            (stakeholders[meatCarrierAccount].profession == 4) && (stakeholders[brandAccount].profession == 5))
+    function createBusiness(address foodProviderID, address breederID, address pigCarrierID, 
+                            address slaughterHouseID, address meatCarrierID, address brandID) {
+        if ((stakeholders[foodProviderID].profession == 0) && (stakeholders[breederID].profession == 1) &&
+            (stakeholders[pigCarrierID].profession == 2) && (stakeholders[slaughterHouseID].profession == 3) &&
+            (stakeholders[meatCarrierID].profession == 4) && (stakeholders[brandID].profession == 5))
           
-            businessList[businessID++] = Business(foodProviderAccount, breederAccount, pigCarrierAccount, 
-                            slaughterHouseAccount, meatCarrierAccount, brandAccount);
+            businessProcessList[businessProcessID++] = BusinessProcess(foodProviderID, breederID, pigCarrierID, 
+                            slaughterHouseID, meatCarrierID, brandID, 0);
     }
     
     /* Reputate a stakeholder (evaluated) for a business with a score from 0 to 3. */
-    function reputate(uint businessID, address evaluatedAccount, uint score) {
-        address evaluatorAccount = msg.sender;
-        Stakeholder evaluator = stakeholders[evaluatorAccount];
-        Stakeholder evaluated = stakeholders[evaluatedAccount];
+    function reputate(uint businessProcessID, address evaluatedID, uint score) {
+        address evaluatorID = msg.sender;
+        Stakeholder evaluator = stakeholders[evaluatorID];
+        Stakeholder evaluated = stakeholders[evaluatedID];
         
-        if (canReputate(businessID, evaluator, evaluated)) {
-            Evaluation evaluation = Evaluation(getReputation(evaluatorAccount), score);
-            evalutions[evaluatedAccount].add(evaluation);
+        if (canReputate(businessProcessID, evaluator, evaluated)) {
+            uint reputationOfEvaluator = getReputation(evaluatorID);
+            Evaluation evaluation = Evaluation(businessProcessID, evaluatorID, evaluatedID, reputationOfEvaluator, score);
+            evaluations[evaluationCount++] = evaluation;
         }
     }
     
     /* Check if the evaluator can reputate the evaluated for the business */ 
-    function canReputation(uint businessID, address evaluatorAccount, address evaluatedAccount) constant returns (bool result) {
-        // check if these stakeholder are in this business
+    function canReputate(uint businessProcessID, Stakeholder evaluator, Stakeholder evaluated) private constant returns (bool result) {
+        // check if these stakeholders are in this business
+        bool b1 = isInsideBusinessProcess(businessProcessID, evaluator.id);
+        bool b2 = isInsideBusinessProcess(businessProcessID, evaluated.id);
         
-        // check if the evaluator has not already reputated the evaluated
+        // check if the evaluator has not already reputated the evaluated // TODO
         
         // check if evaluator can evaluate the evaluated regarding to the relation rules
+        bool b3 = evaluationRelationships[evaluator.profession][evaluated.profession];
         
         // if all are true return true, false otherwise.
+        return (b1 && b2 && b3);
     }
     
+    function isInsideBusinessProcess(uint businessProcessID, address stakeholderID) constant returns (bool result) {
+        BusinessProcess businessProcess = businessProcessList[businessProcessID];
+        Stakeholder stakeholder = stakeholders[stakeholderID];
+        bool c0 = (stakeholder.profession == 0) && (stakeholderID == businessProcess.foodProviderID);
+        bool c1 = (stakeholder.profession == 1) && (stakeholderID == businessProcess.breederID);
+        bool c2 = (stakeholder.profession == 2) && (stakeholderID == businessProcess.pigCarrierID);
+        bool c3 = (stakeholder.profession == 3) && (stakeholderID == businessProcess.slaughterHouseID);
+        bool c4 = (stakeholder.profession == 4) && (stakeholderID == businessProcess.meatCarrierID);
+        bool c5 = (stakeholder.profession == 5) && (stakeholderID == businessProcess.brandID);
+        return (c0 || c1 || c2 || c3 || c4 || c5);
+    }    
+    
     /* Calculate and return the reputation value for the given stakeholder. */
-    function getReputation(address stakeholderAccount) constant returns (uint reputation) {
+    function getReputation(address stakeholderID) constant returns (uint reputation) {
         uint totalWeight = 0;
         uint totalWeightedScore = 0;
-        Evaluation[] evaluationList = evaluations[stakeholderAccount];
         
-        for (int i=0; i<evaluationList.size-1; i++) {
-            Evaluation evaluation = evaluationList[i];
-            totalWeight += evaluation.weight;
-            totalWeightedScore += evaluation.weight * evaluation.score;
+        for (uint i = 0; i < evaluationCount - 1; i++) {
+            Evaluation evaluation = evaluations[i];
+            if (evaluation.evaluatedID == stakeholderID) {
+               totalWeight += evaluation.weight;
+               totalWeightedScore += evaluation.weight * evaluation.score;
+            }
         }
         
-        return round(totalWeightedScore/totalWeight);
+        return totalWeightedScore/totalWeight;
     }
 }
