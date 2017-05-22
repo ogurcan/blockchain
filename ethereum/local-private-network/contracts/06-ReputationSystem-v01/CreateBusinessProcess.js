9ef835affcfb6b3c711df6157d57e0b3b6304b76
Önder Gürcan <onder.gurcan@gmail.com>
2017-05-22 14:59:19 +0300

    Update CreateBusinessProcess.js

--- ethereum/local-private-network/contracts/06-ReputationSystem-v01/CreateBusinessProcess.js
+++ ethereum/local-private-network/contracts/06-ReputationSystem-v01/CreateBusinessProcess.js
@@ -1,4 +1,4 @@
 // unlock all accounts
 loadScript("config/UnlockAccounts.js");
 // now create the business process
-reputationSystem.createBusinessProcess(eth.accounts[0], eth.accounts[1], eth.accounts[2], eth.accounts[3], eth.accounts[4], eth.accounts[5], {from: eth.coinbase, gas: 100000});
+reputationSystem.createBusinessProcess(eth.accounts[0], eth.accounts[1], eth.accounts[2], eth.accounts[3], eth.accounts[4], eth.accounts[5], {from: eth.coinbase, gas: 300000});
